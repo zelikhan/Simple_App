97b07fe735decd63410504d4650853cde359f362
Muzammil Shoukat <114800813+zelikhan@users.noreply.github.com>
2022-10-16 11:10:32 +0500

    initital commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home : Material(
+        child:Center(
+          child: Container(
+      child: Text("Hello World ts of first flutter Application"),
+          ),
+        ),
+      ),
+    );
+  }
+}
